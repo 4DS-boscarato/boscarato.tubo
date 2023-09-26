6d863668f0c2164f5742e210131c8af934ef5b1d
andrea <andrea.boscarato@buonarroti.tn.it>
2023-09-26 18:37:22 +0200

    Commit Finale

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,15 @@ public class Main {
         int altezza;
         int raggio;
 
-        System.out.println("Inserisci l'altezza del tubo: ");
+        System.out.print("Inserisci l'altezza del tubo: ");
         altezza = input.nextInt();
 
-        System.out.println("Inserisci il raggio del tubo: ");
+        System.out.print("Inserisci il raggio del tubo: ");
         raggio = input.nextInt();
 
         Tubo t1 = new Tubo(altezza, raggio);
 
-        System.out.println("La superfice totale del tubo e`: " + t1.superficeTot());
-        System.out.println("il volume del tubo e`: " + t1.volume());
-
+        System.out.println("\nLa superfice totale del tubo e`: " + t1.superficeTot());
+        System.out.println("Il volume del tubo e`: " + t1.volume());
     }
 }
