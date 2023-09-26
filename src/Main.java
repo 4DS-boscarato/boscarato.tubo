4fc629b9d4b31203c1f00f01ffe7a0718af873e8
andrea <andrea.boscarato@buonarroti.tn.it>
2023-09-26 18:29:28 +0200

    Test: Valori per dichiarazione Costruttore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        int altezza;
+        int raggio;
+
+        System.out.println("Inserisci l'altezza del tubo: ");
+        altezza = input.nextInt();
+
+        System.out.println("Inserisci il raggio del tubo: ");
+        raggio = input.nextInt();
+
+        Tubo t1 = new Tubo(altezza, raggio);
     }
 }
